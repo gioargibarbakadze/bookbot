f6b476dead0ea2c685256d3c919743bdb5c69286
gioargibarbakadze <barbakadzegiorga0@gmail.com>
2024-09-04 16:54:51 +0400

    counted the character

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = word_count(text)
-    print(f"{num_words} words")
+    #num_words = word_count(text)
+    letters_dict = count_letters(text)
+    print(letters_dict)
+    #print(f"{num_words} words")
     #print(text)
 
 
@@ -13,6 +15,18 @@ def get_book_text(path):
 def word_count(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    lowered_string = text.lower()
+    dic = {}
+    for char in lowered_string:
+        if char in dic:
+            dic[char] += 1
+        else:
+            dic[char] = 1
+    return dic
+
+
 
 
 
